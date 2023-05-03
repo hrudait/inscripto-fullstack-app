b591df576e739fb955ff1e3379d769a2b08cfd0f
hrudait <hrudai.t@outlook.com>
2023-05-02 20:02:40 -0700

    eehhe

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000
 app.listen(port)
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 app.use(cors())
 app.get('/', (req,res) =>{
     res.json({bruh:"hello"})
