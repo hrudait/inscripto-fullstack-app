0be759ea65e23943ff10ec53a1f1a60cfb2b1bd6
hrudait <hrudai.t@outlook.com>
2023-05-02 19:39:50 -0700

    eehhe

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-port = 5000
+const port = process.env.PORT || 5000
 app.listen(port)
 app.use(cors())
 app.get('/', (req,res) =>{
