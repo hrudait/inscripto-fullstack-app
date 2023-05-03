1806bd32c33f1bd5e9281def73104bb2468469dd
hrudait <hrudai.t@outlook.com>
2023-05-02 19:30:25 -0700

    eehhe

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-
+import { useState,useEffect } from "react"
 function App() {
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/").then(
+      response => response.json()
+    ).then(
+      data=>{
+        setBackendData(data)
+      }
+    )
+  },[])
   return (
     <div className="App">
       <h1>
-        HEhehehe
-        
+        {backendData.bruh}
       </h1>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express();
-
-app.listen(5000)
-
+const cors = require('cors')
+port = 5000
+app.listen(port)
+app.use(cors())
 app.get('/', (req,res) =>{
     res.json({bruh:"hello"})
 })
