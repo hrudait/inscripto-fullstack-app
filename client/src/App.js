331b19c5cac2f366ee09856bc284f1db727d7827
hrudait <hrudai.t@outlook.com>
2023-05-02 20:45:01 -0700

    eehhe

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@ import { useState,useEffect } from "react"
 function App() {
   const [backendData, setBackendData] = useState([{}])
 
-  useEffect(()=>{
-    fetch("https://backend-kn3o.onrender.com/").then(
-      response => response.json()
-    ).then(
-      data=>{
-        setBackendData(data)
-      }
-    )
-  },[])
+  // useEffect(()=>{
+  //   fetch("https://backend-kn3o.onrender.com/").then(
+  //     response => response.json()
+  //   ).then(
+  //     data=>{
+  //       setBackendData(data)
+  //     }
+  //   )
+  // },[])
   return (
     <div className="App">
       <h1>
-        {backendData.bruh}
+        EHhehe
       </h1>
     </div>
   );
