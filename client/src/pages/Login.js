c71efe8b2329eff6a0d79d52d285d0fa578f51ea
Hrudai Thungathurthi <hrudai@hrudai.com>
2023-07-06 23:08:49 -0700

    new login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -31,133 +31,122 @@ function Login(){
     }
     const css = `
     
-  body {
-    font-size: 16px;
-    background: rgba(86,31,157,1);
-  }
-  .box {
-    width: 30rem;
-    height: 25rem;
-    background: rgba(224,223,223,1);
-    opacity: 1;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    border-top-left-radius: 27px;
-    border-top-right-radius: 27px;
-    border-bottom-left-radius: 27px;
-    border-bottom-right-radius: 27px;
-    overflow: hidden;
-  }
-  .logintext {
-    width:100%;
-    color: rgba(0,0,0,1);
-    position: absolute;
-    font-family: DM Sans;
-    font-weight: Regular;
-    font-size: 7rem;
-    opacity: 1;
-    text-align: center;
-  }
-  .loginbutton {
-    width: 90%;
-    height: 10%;
-    position: absolute;
-    top: 85%;
-    left: 5%;
-    border-top-left-radius: 50vw;
-    border-top-right-radius: 50vw;
-    border-bottom-left-radius: 50vw;
-    border-bottom-right-radius: 50vw;
-    border: 0;
-    background-color: rgba(86,31,157,1);
-    
-    overflow: hidden;
-  }
-  .loginbutton:hover{
-    opacity: 0.8;
-  }
-  .loginbuttontext{
-    color: white;
-    font-size: 2rem;
-  }
-  .username{
-    position: absolute;
-    width: 90%;
-    height: 10%;
-    top: 40%;
-    left: 5%;
-    font-size: 1.75rem;
-    padding-left: 1%;
-    padding-top: .5%;
-  }
-  .email{
-    position: absolute;
-    width: 90%;
-    height: 10%;
-    top: 40%;
-    left: 5%;
-    font-size: 1.75rem;
-    padding-left: 1%;
-    padding-top: .5%;
-  }
-  .password{
-    position: absolute;
-    width: 90%;
-    height: 10%;
-    top: 55%;
-    left: 5%;
-    font-size: 1.75rem;
-    padding-left: 1%;
-    padding-top: .5%;
-    font-family: Geologica;
-    font-weight: Regular;
-    padding-bottom: 0%;
-  }
-  .forgot{
-    position: absolute;
-    top: 75%;
-    left: 5%;
-    font-size: 1.5rem;
-    font-family: Geologica;
-  }
-  .login{
-    position: absolute;
-    top: 75%;
-    left: 5%;
-    font-size: 1.5rem;
-    font-family: Geologica;
-  }
-  .register{
-    position: absolute;
-    top: 75%;
-    right: 5%;
-    font-size: 1.5rem;
-    font-family: Geologica;
-  }
+  
+body{
+  background-color: black;
+}
+.box{
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+
+  align-items: center;
+}
+.welcome{   
+  color: white;
+  margin: 0;
+  text-align: center;
+  font-size: 3vw;
+  font-family: 'Open Sans';
+  font-weight: 400;
+  margin-bottom: 2vw;
+  margin-left: 8vw;
+  margin-right: 8vw;
+}
+.username{
+  background-color: black;
+  color: white;
+  border: .25vw solid white;
+  border-radius: .75vw;
+  width: 97.5%;
+  height: 5vw;
+  font-size: 1.66vw;
+  font-family: 'Open Sans';
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+  padding-left: 2.5%;
+  margin-bottom: 1vw;
+  outline-color: white;
+  
+}
+.password{
+  background-color: black;
+  color: white;
+  border: .25vw solid white;
+  border-radius: .75vw;
+  width: 97.5%;
+  height: 5vw;
+  font-size: 1.66vw;
+  font-family: 'Open Sans';
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+  outline-color: white;
+  padding-left: 2.5%;
+}
+.linkcontainer{
+  margin-left: 1px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1vw;
+}
+.forgot{
+  color: white;
+  font-family: 'Open Sans';
+  font-weight: 400;
+  font-size: 1.66vw;
+}
+.register{
+  color: white;
+  font-family: 'Open Sans';
+  font-weight: 400;
+  font-size: 1.66vw;
+}
+.loginbutton {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+  margin: 0 auto;
+  margin-top: 1vw;
+  height: 5vw;
+  border: 0;
+  border-radius: .5vw;
+}
+.loginbuttontext{
+  color: black;
+  font-family: 'Open Sans';
+  font-weight: 400;
+  font-size: 3vw;
+}
+.loginbutton:hover{
+  opacity: 0.8;
+}
     `
     return(
-        <div className='loginpage'>
+      <div className='loginpage'>
             <style>
                 {css}
             </style>
-            <div className="box">
-                <span className="logintext">LOGIN</span>
-                <div className="loginform">
-                    <form onSubmit={Submit}>
-                        <input className = "username" id="username"type="text" title="username" placeholder="Username"  required/><br />
-                        <input className = "password" id="password"type="password" title="password" placeholder="Password" required/><br />
-                        <div className='linkcontainer'>
-                            <a className='forgot' href='/forgot'>Forgot Password</a>
-                            <a className='register' href='/register'>Create An Account</a>
-                        </div>
-                        <button type='submit' className="loginbutton"><label className='loginbuttontext'>Login</label></button>
-                    </form>
-                </div>
+        <div className="box">
+            <h1 className="welcome">Welcome Back!</h1>
+            <div className="loginform">
+                <form onSubmit={Submit}>
+                    <input className = "username" id="username"type="text" title="username" placeholder="Username"  required/><br />
+                    <input className = "password" id="password"type="password" title="password" placeholder="Password" required/><br />
+                    <div className='linkcontainer'>
+                        <a className='forgot' href='/forgot'>Forgot Password</a>
+                        <a className='register' href='/register'>Create An Account</a>
+                    </div>
+                    <button type='submit' className="loginbutton"><label className='loginbuttontext'>login</label></button>
+                </form>
             </div>
         </div>
+      </div>
     )
 }
 export default Login;
