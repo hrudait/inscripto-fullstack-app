6b9026e4341feaae31d1980f52fe311dab600bc4
Hrudai Thungathurthi <hrudai@hrudai.com>
2023-07-12 23:27:16 -0700

    css pages done for desktop for all current pages, still need forgot password

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAuthHeader, useAuthUser } from "react-auth-kit";
-
+import logo from './logo.svg'
 
 function Home(){
     const [credits, setcredits] = useState();
@@ -92,9 +92,9 @@ function Home(){
             <div>
                 {
                     currentData.map( csvitem => (
-                        <div key={csvitem.id}>
-                            <p>Search Term {csvitem.name}</p>
-                            <p>Location {csvitem.location}</p>
+                        <div className = "currentitem" key={csvitem.id}>
+                            <p className="currentsearchTerm">Search Term: {csvitem.name}</p>
+                            <p className="currentlocation">Location: {csvitem.location}</p>
                         </div>
                     ))
                 }
@@ -123,46 +123,323 @@ function Home(){
             <div>
                 {
                     finishedData.map( csvitem => (
-                        <div key={csvitem.id}>
-                            <p>Search Term {csvitem.name}</p>
-                            <p>Location {csvitem.location}</p>
-                            <a href={csvitem.url}>CSV Download</a>
+                        <div className="finisheditem" key={csvitem.id}>
+                            <p className="finishedsearchTerm">Search Term: {csvitem.name}</p>
+                            <p className="finishedlocation">Location: {csvitem.location}</p>
+                            <a className="finisheddownlaod"href={csvitem.url}>CSV Download</a>
                         </div>
                     ))
                 }
-                <div>
-                {hideLeftA ? null : <span id="lefta" onClick={()=>decPage()}>&lt;</span>}
-                <span>{page}</span>
-                {hideRightA ? null : <span id="righta" onClick={()=>incPage()}>&gt;</span>}
+                <div className="pagechanger">
+                {hideLeftA ? null : <span className="lefta" id="lefta" onClick={()=>decPage()}>&lt;</span>}
+                <span className="pagenum">{page}</span>
+                {hideRightA ? null : <span id="righta" className="righta" onClick={()=>incPage()}>&gt;</span>}
                     
                 </div>
             </div>
         )
     }
 
+    const css = `
+    .pagechanger{
+        display: flex;
+      justify-content: space-evenly;
+      margin: 0;
+    }
+    .pagenum{
+        color: white;
+        font-family: 'Open Sans';
+        font-weight: 400;
+        font-size: 1.66vw;
+    }
+    .righta{
+        color: white;
+        font-family: 'Open Sans';
+        font-weight: 400;
+        font-size: 1.66vw;
+        margin-left: 1vw;
+        margin-right: 1vw;
+    }
+    .lefta{
+        color: white;
+        font-family: 'Open Sans';
+        font-weight: 400;
+        font-size: 1.66vw;
+        margin-left: 1vw;
+        margin-right: 1vw;
+    }
+    .lefta:hover{
+        text-decoration: underline;
+    }
+    .righta:hover{
+        text-decoration: underline;
+    }
+    body{
+        background-color: black;
+      }
+    .logo{
+      width: 20vw;
+      margin: 0;
+    }
+    .navbar{
+      display: flex;
+      justify-content: space-evenly;
+      margin: 0;
+      margin-bottom: 2vw;
+    }
+    .navhome{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-right: 3.5vw;
+    }
+    .navhome:hover{
+      opacity: 0.8;
+    }
+    .navreload{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-right: 4.5vw;
+    }
+    .navreload:hover{
+      opacity: 0.8;
+    }
+    .credits{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;  
+    }
+    .container{
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      
+    }
+    .startandnow{
+      
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      box-sizing: border-box;
+      align-items: center;
+    }
+    .finishedbox{
+      
+      display: flex;
+      justify-content: center;
+      box-sizing: border-box;
+      align-items: flex-start; /* Align items at the top */
+    }
+    .finished{
+      
+      margin: 0;
+    }
+    .start{
+      
+      margin-bottom: 5vw;
+    }
+    .starttext{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-left: 2.5vw;
+      margin-right: 2.5vw;
+      margin-bottom: 1vw;
+    }
+    .searchTermtext{
+      color: white;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-right: 4.5vw;
+    }
+    .locationtext{
+      color: white;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-right: 4.5vw;
+    }
+    .searchTerm{
+      background-color: black;
+      color: white;
+      border: .25vw solid white;
+      width: 100%;
+      height: 3vw;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      padding: 0;
+      padding-left: 2.5%;
+      margin-bottom: 1vw;
+      outline-color: white;
+      box-sizing: border-box;
+      
+    }
+    .location{
+      background-color: black;
+      color: white;
+      border: .25vw solid white;
+      width: 100%;
+      height: 3vw;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      padding: 0;
+      padding-left: 2.5%;
+      margin-bottom: 1vw;
+      outline-color: white;
+      box-sizing: border-box;
+      
+    }
+    .current{
+      
+    }
+    .runningnow{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-left: .5vw;
+      margin-right: .5vw;
+      margin-bottom: 1vw;
+    }
+    .finishedtext{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-left: 8vw;
+      margin-right: 8vw;
+      margin-bottom: 1vw;
+    }
+    .startbutton {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 40%;
+      margin: 0 auto;
+      height: 2vw;
+      border: 0;
+      border-radius: .5vw;
+    }
+    .startbuttontext{
+      color: black;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      font-size: 1.66vw;
+    }
+    .startbutton:hover{
+      opacity: 0.8;
+    }
+    .currentitem{
+      width: 100%;
+      box-sizing: border-box;
+      margin: 0;
+      border: .25vw solid white;
+      border-radius: .5vw;
+      margin-bottom: 1vw;
+    }
+    .currentsearchTerm{
+      color: white;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      padding-left: .25vw;
+    }
+    .currentlocation{
+      color: white;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      padding-left: .25vw;
+    }
+    .finisheditem{
+      width: 100%;
+      box-sizing: border-box;
+      margin: 0;
+      border: .25vw solid white;
+      border-radius: .5vw;
+      margin-bottom: 1vw;
+    }
+    .finishedsearchTerm{
+      color: white;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      padding-left: .25vw;
+    }
+    .finishedlocation{
+      color: white;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      padding-left: .25vw;
+    }
+    .finisheddownlaod{
+      color: white;
+      font-size: 1vw;
+      font-family: 'Open Sans';
+      text-decoration: underlines;
+      font-weight: 400;
+      margin: 0;
+      padding-left: .25vw;
+    }`
     return(
+        
         <div>
-            <h1>Home Page</h1>
-            <h1>Current number of credits {credits}</h1>
-            <button onClick={reload}>Reload Credits</button>
-            <form onSubmit={submit}>
-                <label for='searchTerm'>Search Term &#40;Just the type of business you are looking for&#41;</label>
-                <br></br>
-                <input id = "searchTerm" type="text" title="searchTerm" placeholder="Ex: restaurants"  required/><br />
-                <label for='location'>Location</label>
-                <br></br>
-                <span>Format: Street&#40;Optional&#41;, City, State, Country</span>
-                <br></br>
-                <input id = "location" type="text" title="Location" placeholder="Ex: Seattle, WA, USA"  required/><br />
-                <button type="submit" className="btn">Submit</button><br />
-            </form>
-            <div>
-                <h1>Currently Running(Limit of 5)</h1>
-                <CurrentCsvs/>
-            </div>
-            <div>
-                <h1>Finished Runs</h1>
-                <FinishedCsvs/>
+            <style>
+                {css}
+            </style>
+            <header class="navbar">
+                <img class ="logo"src={logo}/>
+                <nav>
+                    <a href="/" class="navhome">Home</a>
+                    <a href="/reload" class="navreload">Reload</a>
+                </nav>
+                <h1 class="credits">credits: {credits}</h1>
+            </header>
+            <div class="container">
+                <div class="startandnow">
+                    <div class="start">
+                        <form onSubmit={submit}>
+                        <h1 class="starttext">Start a Search</h1>
+                        <h2 class="searchTermtext">Search Term (one per search):</h2>
+                        <input class = "searchTerm" id="searchTerm"type="text" title="searchTerm" placeholder="Ex: Restaurants, Painters, etc."  required/><br />
+                        <h2 class="locationtext">Location:</h2>
+                        <input class = "location" id="location" type="text" title="location" placeholder="Format: Street(Optional), City, State, Country"  required/><br />
+                        <button class = 'startbutton' type="submit"><span class="startbuttontext">start</span></button>
+                        </form>
+                    </div>
+                    <div class="current">
+                        <h1 class="runningnow">Running Now (Limit 5)</h1>
+                        <CurrentCsvs/>
+                    </div>
+                </div>
+                <div class="finishedbox">
+                    <div class="finished">
+                        <h1 class="finishedtext">Finished Runs</h1>
+                        <FinishedCsvs/>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- client/src/pages/Reload.js
+++ client/src/pages/Reload.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import {useAuthUser, useAuthHeader} from 'react-auth-kit'
 import axios from 'axios'
+import logo from './logo.svg'
 function Reload(){
     const [credits, setcredits] = useState();
     const auth = useAuthUser()
@@ -45,21 +46,182 @@ function Reload(){
         event.preventDefault();
         next(parseInt(event.target.creditAmount.value))
     }
+
+    const css = `body{
+        background-color: black;
+      }
+    .logo{
+      width: 20vw;
+      margin: 0;
+    }
+    .navbar{
+      display: flex;
+      justify-content: space-evenly;
+      margin: 0;
+    }
+    .navhome{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-right: 3.5vw;
+    }
+    .navhome:hover{
+      opacity: 0.8;
+    }
+    .navreload{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      margin-right: 4.5vw;
+    }
+    .navreload:hover{
+      opacity: 0.8;
+    }
+    .credits{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;  
+    }
+    .choose{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0; 
+      text-align: center;
+      margin: 2.5vw;
+    }
+    .customtext{
+      color: white;
+      font-size: 3vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0; 
+      text-align: center;
+      margin-bottom: 5vw;
+    }
+    .checkout {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 60%;
+      margin: 0 auto;
+      margin-top: 1vw;
+      height: 5vw;
+      border: 0;
+      border-radius: .5vw;
+    }
+    .checkoutText{
+      color: black;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      font-size: 3vw;
+    }
+    .checkout:hover{
+      opacity: 0.8;
+    }
+    .inputbox{
+      background-color: black;
+      color: white;
+      border: .25vw solid white;
+      border-radius: .75vw;
+      width: 30vw;
+      height: 3vw;
+      font-size: 1.66vw;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      margin: 0;
+      padding: 0;
+      margin-bottom: 5vw;
+      margin-left: auto;
+      margin-right: auto;
+      outline-color: white;
+    }
+    .container{
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      align-items: center;
+    }
+    .buttons {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 0;
+      margin-bottom: 2.5vw;
+    }
+    .topthree{
+      display: flex;
+      justify-content: space-evenly;
+      margin: 0;
+      margin-bottom: .5vw;
+    }
+    .bottomthree{
+      display: flex;
+      justify-content: space-evenly;
+      margin: 0;
+    }
+    .packs{
+      width: 15vw;
+      background-color: white;
+      font-family: 'Open Sans';
+      font-weight: 400;
+      font-size: 2vw; 
+      margin: 0;
+      border: 0;
+      margin-left: .25vw;
+      margin-right: .25vw;
+    }
+    .packs:hover{
+      opacity: 0.8;
+    }`
+
     return(
         <div>
-            <h1>Current number of credits {credits}</h1>
-            <div className="choose">
-                <p>Choose an Amount</p>
-                <button onClick={()=>next(5)}>5 credits</button>
-                <button onClick={()=>next(10)}>10 credits</button>
-                <button onClick={()=>next(25)}>25 credits</button>
-                <button onClick={()=>next(50)}>50 credits</button>
-                <button onClick={()=>next(100)}>100 credits</button>
-                <p>Or Choose a number</p>
-                <form onSubmit={nextInput}>
-                    <input name = "creditAmount" type="text"  onInput={handleChange} />
-                    <button type="submit">Checkout</button>
-                </form>
+            <style>
+                {css}
+            </style>
+            <header className="navbar">
+                <img className ="logo"src={logo}/>
+                <nav>
+                    <a href="/" className="navhome">Home</a>
+                    <a href="/reload" className="navreload">Reload</a>
+                </nav>
+                <h1 className="credits">credits: {credits}</h1>
+            </header>
+            <div>
+                <div className="container">
+                    <h1 className="choose">Choose A Package</h1>
+                    <div className="buttons">
+                        <div className="topthree">
+                            <button onClick={()=>next(5)} className="packs">5 credits</button>
+                            <button onClick={()=>next(10)} className="packs">10 credits</button>
+                            <button onClick={()=>next(25)} className="packs">25 credits</button>
+                        </div>
+                        <div className="bottomthree">
+                            <button onClick={()=>next(50)} className="packs">50 credits</button>
+                            <button onClick={()=>next(100)} className="packs">100 credits</button>
+                            <button onClick={()=>next(200)} className="packs">200 credits</button>
+                        </div>
+                    </div>
+                    <h1 className="customtext">Or Choose Custom Amount</h1>
+                    <form onSubmit={nextInput}>
+                        <input type ="text" onInput={handleChange} className="inputbox" name = "creditAmount"/>
+                        <button className = 'checkout' type="submit"><span className="checkoutText">checkout</span></button>
+                    </form>
+                </div>
             </div>
         </div>
         
